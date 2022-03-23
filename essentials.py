2902a51aad09432c1075546e96ec4ac3475d5f03
zenbook <mrgulin1@gmail.com>
2022-03-23 12:27:49 +0100

    file reformatted, added generate_huckel_hamiltonian funciton

--- essentials.py
+++ essentials.py
@@ -4,6 +4,7 @@ import numpy as np
 OUTPUT_FORMATTING_NUMBER = "+8.3f"
 OUTPUT_SEPARATOR = " "
 
+
 def print_matrix(matrix, plot_heatmap='', ret=False):
     ret_string = ""
     for line in matrix:
@@ -18,6 +19,7 @@ def print_matrix(matrix, plot_heatmap='', ret=False):
         return ret_string
     print(ret_string, end='')
 
+
 def generate_1rdm(Ns, Ne, wave_function):
     # generation of 1RDM
     if Ne % 2 != 0:
@@ -28,3 +30,19 @@ def generate_1rdm(Ns, Ne, wave_function):
         vec_i = wave_function[:, k][np.newaxis]
         y += vec_i.T @ vec_i  #
     return y
+
+
+def generate_huckel_hamiltonian(n_s, number_of_electrons=2, t=1):
+    h = np.zeros((n_s, n_s), dtype=np.float64)  # reinitialization
+    if (number_of_electrons / 2) % 2 == 0:
+        h[0, n_s - 1] = t
+        h[n_s - 1, 0] = t
+        pl = "ANTIPERIODIC" + '\n'
+    else:
+        h[0, n_s - 1] = -t
+        h[n_s - 1, 0] = -t
+        pl = "PERIODIC" + '\n'
+
+    h += np.diag(np.full((n_s - 1), -t), -1) + np.diag(np.full((n_s - 1), -t), 1)
+    print(pl)
+    return h
