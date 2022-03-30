41b6cd78ea0eb929f246a5e3382e525a5f684eaf
zenbook <mrgulin1@gmail.com>
2022-03-30 11:12:15 +0200

    New types of molecules

--- LPFET/calculate_batch.py
+++ LPFET/calculate_batch.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import typing
 
 sys.path.append('../')
 sys.path.append(r'C:\Users\tinc9\Documents\CNRS-offline')
@@ -19,18 +20,105 @@ importlib.reload(lpfet)
 import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def generate_chain_const_u(U_param, n_electron, n_sites, delta_x=0.1):
+
+def generate_chain1(i, n_sites, U_param):
+    nodes_dict = dict()
+    edges_dict = dict()
+    eq_list = []
+    for j in range(n_sites):
+        nodes_dict[j] = {'v': (j - 2.5) * i, 'U': U_param}
+        if j != 5:
+            edges_dict[(j, j + 1)] = 1
+        eq_list.append([j])
+    return nodes_dict, edges_dict, eq_list
+
+
+def generate_chain2(i, n_sites, U_param):
+    nodes_dict = dict()
+    edges_dict = dict()
+    eq_list = []
+    for j in range(n_sites):
+        nodes_dict[j] = {'v': 0, 'U': U_param}
+        if j != 5:
+            edges_dict[(j, j + 1)] = 1
+        eq_list.append([j])
+    nodes_dict[0]['v'] = i
+    return nodes_dict, edges_dict, eq_list
+
+
+def generate_chain3(i, n_sites, U_param):
+    nodes_dict = dict()
+    edges_dict = dict()
+    eq_list = []
+    for j in range(n_sites):
+        nodes_dict[j] = {'v': i * (-1) ** j, 'U': U_param}
+        if j != 5:
+            edges_dict[(j, j + 1)] = 1
+        eq_list.append([j])
+    return nodes_dict, edges_dict, eq_list
+
+
+def generate_star1(i, n_sites, U_param):
+    nodes_dict = dict()
+    edges_dict = dict()
+    eq_list = [[0], list(range(1, n_sites))]
+    for j in range(n_sites):
+        nodes_dict[j] = {'v': 0, 'U': U_param}
+        if j != 0:
+            edges_dict[(0, j)] = 1
+    nodes_dict[0]['v'] = i
+
+    return nodes_dict, edges_dict, eq_list
+
+
+def generate_complete1(i, n_sites, U_param):
+    nodes_dict = dict()
+    edges_dict = dict()
+    eq_list = [[0]]
+    stop1 = False
+    for j in range(n_sites):
+        nodes_dict[j] = {'v': 0, 'U': U_param}
+        for k in range(j + 1, n_sites):
+            edges_dict[(j, k)] = 1
+        if j != 0 and not stop1:
+            if j != n_sites - j:
+                eq_list.append([j, n_sites - j])
+            else:
+                eq_list.append([j])
+                stop1 = True
+    nodes_dict[0]['v'] = i
+
+    return nodes_dict, edges_dict, eq_list
+
+
+def generate_trend(n_sites, n_electron, model_function: typing.Callable, molecule_name, u_param=None, i_param=None,
+                   delta_x=None, max_value=None):
     lpfet.COMPENSATION_5_FACTOR = 1
     lpfet.COMPENSATION_5_FACTOR2 = 1
     lpfet.COMPENSATION_1_RATIO = 0.25
+    name = molecule_name
+    if u_param is not None and i_param is not None:
+        raise Exception("Can't specify both parameters")
+    elif u_param is not None:
+        # U is set
+        if delta_x is None:
+            delta_x = 0.1
+            max_value = 2
+        x = np.arange(0, max_value + 0.01, delta_x)
+        folder_name = f'results/i_{name}_ns-{n_sites}_ne-{n_electron}_u-{u_param}_dist-{delta_x}/'
+        constant_var = 'u'
 
-    x = np.arange(0, 2 + delta_x / 2, delta_x)
-    # delta_x = 0.1
-    # U_param = 8
-    # n_electron = 6
-    # n_sites = 6
-    name = 'chain1'
-    folder_name = f'results/{name}_ns-{n_sites}_ne-{n_electron}_u-{U_param}_dist-{delta_x}/'
+    elif i_param is not None:
+        # i is set
+        if delta_x is None:
+            delta_x = 0.5
+            max_value = 10
+        x = np.arange(0, max_value + 0.01, delta_x)
+        folder_name = f'results/u_{name}_ns-{n_sites}_ne-{n_electron}_i-{i_param}_dist-{delta_x}/'
+        constant_var = 'i'
+    else:
+        raise Exception("None of the parameteres is set!")
+
     for one_path in [folder_name, folder_name + 'v_hxc', folder_name + 'occupation']:
         if not os.path.isdir(one_path):
             os.mkdir(one_path)
@@ -47,22 +135,24 @@ def generate_chain_const_u(U_param, n_electron, n_sites, delta_x=0.1):
     energy_per_site = []
     energy_ref_per_site = []
     correction_dict_list = []
+    old_v_hxc = np.zeros(n_sites)
+    with open(f"{folder_name}systems.txt", "w") as myfile:
+        myfile.write("")
     for i in x:
+        if constant_var == "i":
+            u_param = i
+        elif constant_var == "u":
+            i_param = i
         if not first:
             mol1.clear_object(name)
+            mol1.v_hxc = old_v_hxc
         first = False
-        print(f'{i:.1f}, {i / max(x) * 100:.1f}%: ', end=' ')
-
-        nodes_dict = dict()
-        edges_dict = dict()
-        eq_list = []
-        for j in range(6):
-            nodes_dict[j] = {'v': (j - 2.5) * i, 'U': U_param}
-            if j != 5:
-                edges_dict[(j, j + 1)] = 1
-            eq_list.append([j])
+        print(f'\n\n{i:.1f}, {i / max(x) * 100:.1f}%: ', end=' ')
+        nodes_dict, edges_dict, eq_list = model_function(i_param, n_sites, u_param)
         t, v_ext, u = lpfet.generate_from_graph(nodes_dict, edges_dict)
         mol1.report_string = f'Object with {n_sites} sites and {n_electron} electrons\n'
+        with open(f"{folder_name}systems.txt", "a") as myfile:
+            myfile.write(f"\n\n{repr(t)}\n{repr(v_ext)}\n{repr(u)}")
         mol1.add_parameters(u, t, v_ext, eq_list)
         mol1.self_consistent_loop(num_iter=50, tolerance=1E-6, oscillation_compensation=[5, 1],
                                   overwrite_output=folder_name + 'log.txt')
@@ -79,81 +169,10 @@ def generate_chain_const_u(U_param, n_electron, n_sites, delta_x=0.1):
         v_hxc_progression_list.append(mol1.v_hxc_progress)
         energy.append(mol1.energy_contributions)
         energy_ref.append(energy_ref_i)
+        old_v_hxc = mol1.v_hxc.copy()
 
     calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_hxc_progression_list,
                      correction_dict_list, energy_per_site, energy_ref_per_site)
-
-
-def generate_chain_const_i(i_param, n_electron, n_sites, delta_x=0.5):
-    lpfet.COMPENSATION_5_FACTOR = 5
-    lpfet.COMPENSATION_5_FACTOR2 = 1
-    lpfet.COMPENSATION_1_RATIO = 0.25
-
-    x = np.arange(0, 10.001, delta_x)
-    # delta_x = 0.1
-    # U_param = 8
-    # n_electron = 6
-    # n_sites = 6
-    name = 'chain1'
-    folder_name = f'results/const-i_{name}_ns-{n_sites}_ne-{n_electron}_i-{i_param}_dist-{delta_x}/'
-    for one_path in [folder_name, folder_name + 'v_hxc', folder_name + 'occupation']:
-        if not os.path.isdir(one_path):
-            os.mkdir(one_path)
-    mol1 = lpfet.Molecule(n_sites, n_electron, name)
-    mol_full = lpfet.class_Quant_NBody.QuantNBody(n_sites, n_electron)
-    mol_full.build_operator_a_dagger_a()
-    y = []
-    v_hxc_progression_list = []
-    y_simple = []
-    y_ref = []
-    energy_ref = []
-    energy = []
-    E_per_site = []
-    energy_per_site = []
-    energy_ref_per_site = []
-    first = True
-    correction_dict_list = []
-    temp_v_hxc = []
-    for i in x:
-
-        if not first:
-            mol1.clear_object(name)
-            mol1.v_hxc = temp_v_hxc
-        first = False
-        print(f'{i:.1f}, {i / max(x) * 100:.1f}%: ', end=' ')
-        print(mol1.v_hxc)
-        nodes_dict = dict()
-        edges_dict = dict()
-        eq_list = []
-        for j in range(6):
-            nodes_dict[j] = {'v': (j - 2.5) * i_param, 'U': i}
-
-            if j != 5:
-                edges_dict[(j, j + 1)] = 1
-            eq_list.append([j])
-        print(nodes_dict)
-        t, v_ext, u = lpfet.generate_from_graph(nodes_dict, edges_dict)
-        mol1.report_string = f'Object with {n_sites} sites and {n_electron} electrons\n'
-        mol1.add_parameters(u, t, v_ext, eq_list)
-        mol1.self_consistent_loop(num_iter=50, tolerance=1E-6, oscillation_compensation=[5, 1],
-                                  overwrite_output=folder_name + 'log.txt')
-        # mol1.optimize_solution(5, 0.2)
-        mol1.calculate_energy(True)
-        temp_v_hxc = mol1.v_hxc.copy()
-        correction_dict_list.append(mol1.oscillation_correction_dict)
-        y.append(mol1.density_progress)
-        y_simple.append(mol1.n_ks)
-        y_ab, mol_fci, energy_ref_i, energy_ref_per_site_i = mol1.compare_densities_FCI(pass_object=mol_full,
-                                                                                        calculate_per_site=True)
-        energy_per_site.append(mol1.per_site_energy)
-        energy_ref_per_site.append(energy_ref_per_site_i)
-        y_ref.append(y_ab.diagonal())
-        v_hxc_progression_list.append(mol1.v_hxc_progress)
-        energy.append(mol1.energy_contributions)
-        energy_ref.append(energy_ref_i)
-
-    calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_hxc_progression_list,
-                     correction_dict_list, energy_per_site, energy_ref_per_site, x_label='U')
 
 
 def calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_hxc_progression_list,
@@ -220,6 +239,7 @@ def calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_h
         box = ax.get_position()
         ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        ax.set_ylim(0, 1)
         plt.savefig(f'{folder_name}/occupation/progression_{x_i}.png', dpi=150, bbox_inches='tight')
         plt.savefig(f'{folder_name}/occupation/progression_{x_i}.svg', dpi=150, bbox_inches='tight')
         plt.close(fig)
@@ -246,7 +266,6 @@ def calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_h
     ax[1].plot(x, E['u'] - E_ref['u'], c=mpl.cm.get_cmap('tab10')(2))
     ax[1].plot(x, E['v_ext'] - E_ref['v_ext'], c=mpl.cm.get_cmap('tab10')(3))
     ax[1].plot(x, [0] * len(x), c='k', linestyle='--')
-    # fig.suptitle(f'{n_sites} sites with U={U_param}, n_e = {n_electron}', fontsize=16)
     fig.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.savefig(f'{folder_name}/Energy_errors.png', dpi=300, bbox_inches='tight')
     plt.savefig(f'{folder_name}/Energy_errors.svg', dpi=300, bbox_inches='tight')
@@ -272,14 +291,18 @@ def calculate_graphs(folder_name, x, y, y_ref, y_simple, energy, energy_ref, v_h
     fig.tight_layout()
     plt.xlabel(x_label)
     ax.set_ylabel('Error energy')
+    ax.set_ylim(0, 1)
     for site in range(6):
         ax.plot(x, E_per_site[:, site]['tot'] - E_ref_per_site[:, site]['tot'], c=mpl.cm.get_cmap('tab10')(site),
                 label=f'deviation on site {site}')
-    # fig.suptitle(f'{n_sites} sites with U={U_param}, n_e = {n_electron}', fontsize=16)
     fig.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     fig.savefig(f'{folder_name}/Energy_errors_per_site.png', dpi=300, bbox_inches='tight')
     fig.savefig(f'{folder_name}/Energy_errors_per_site.svg', dpi=300, bbox_inches='tight')
 
 
 if __name__ == "__main__":
+    # generate_trend(6, 6, generate_chain2, 'chain2', u_param=5)
+    # generate_trend(6, 6, generate_chain3, 'chain3', u_param=5, delta_x=0.4, max_value=4)
+    # generate_trend(6, 6, generate_star1, 'star1', u_param=5, delta_x=0.4, max_value=4)
+    generate_trend(6, 6, generate_complete1, 'complete1', u_param=5, delta_x=0.1, max_value=3)
     pass
