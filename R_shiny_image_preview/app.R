dcbde77643019e48f658b2d2727f80bb182547a6
zenbook <mrgulin1@gmail.com>
2022-03-31 13:08:48 +0200

    Changed name

--- R_shiny_image_preview/app.R
+++ R_shiny_image_preview/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("View data from simulations"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
